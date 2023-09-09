b7100b1be8689929cd3ee2960227d50426efddaa
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-09-09 21:00:44 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    {/* Replace with the URL of your server endpoint that fetches weather data. */}
+    const apiUrl = '/api/weather';
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Real-Time Weather Data</h1>
+      {loading ? (
+        <p>Loading weather data...</p>
+      ) : error ? (
+        <p>Error fetching weather data</p>
+      ) : (
+        <div>
+          <p>Temperature: {weatherData.temp}°C</p>
+          <p>AQI: {weatherData.aqi}</p>
+          <p>Humidity: {weatherData.humidity}%</p>
+          {/* Other weather data fields as needed */}
+        </div>
+      )}
+    </div>
   );
 };
 
