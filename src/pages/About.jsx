e0fdf78e9e6e8e8a23968c9545b192facf97d835
Ayush Thakur <ayushthakur1412@gmail.com>
2023-10-08 18:19:45 +0530

    Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const ComingSoon = () => {
     useEffect(() => {
         function updateCountdown() {
             const currentDate = new Date();
-            const launchDate = new Date("2023-09-31T00:00:00");
+            const launchDate = new Date("2023-11-31T00:00:00");
             const timeDifference = launchDate - currentDate;
 
             const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
