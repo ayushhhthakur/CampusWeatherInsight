55e5f7f5cfe469035eecc13dbcfc50ce4f10771e
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-10-16 21:16:17 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,36 +3,10 @@ import '../styles/Dashboard.css';
 import '../styles/GlobalStyles.css';
 
 const Dashboard = () => {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const url = 'src/data/data.json';
-
-  //     try {
-  //       const response = await fetch(url);
-
-  //       if (!response.ok) {
-  //         throw new Error(`Request failed with status: ${response.status}`);
-  //       }
-
-  //       const result = await response.json();
-  //       setWeatherData(result);
-  //     } catch (error) {
-  //       setError(error.message);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-
-  
   useEffect(() => {
     const fetchData = async () => {
       const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Jammu';
@@ -51,7 +25,10 @@ const Dashboard = () => {
         }
 
         const result = await response.json();
+        console.log('result:', result);
+
         setWeatherData(result);
+        console.log('weatherData:', weatherData);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -61,35 +38,54 @@ const Dashboard = () => {
 
     fetchData();
   }, []);
-  
-  // Filter data based on the selected date
-  const filteredData = weatherData?.filter((dayData) => dayData.date === selectedDate);
 
   return (
     <div className="main-content">
       <h1>Dashboard</h1>
       <div className="dashboard">
-        {/* Date picker component */}
-        <input
-          type="date"
-          value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-        />
-
         {loading && <p className="loading...">Loading weather data....</p>}
         {error && <p className="error">Error: {error}</p>}
-        {filteredData && filteredData.length > 0 && (
+        {weatherData.length > 0 && (
           <div className="weather-data">
-            <h2>Weather Data for {selectedDate}:</h2>
-            <div className="data-row">
-              <div className="data-column">
-                <p>Temperature: {filteredData[0].temp}°C</p>
+            <h2>Weather Data:</h2>
+            {weatherData.map((dayData, index) => (
+              <div key={index} className="data-row">
+                <div className="data-column">
+                  <p>Date: {dayData.date}</p>
+                </div>
+                <div className="data-column">
+                  <p>Temperature: {dayData.temp}°C</p>
+                </div>
+                <div className="data-column">
+                  <p>Feels Like: {dayData.feels_like}°C</p>
+                </div>
+                <div className="data-column">
+                  <p>Cloud Percentage: {dayData.cloud_pct}%</p>
+                </div>
+                <div className="data-column">
+                  <p>Humidity: {dayData.humidity}%</p>
+                </div>
+                <div className="data-column">
+                  <p>Max Temperature: {dayData.max_temp}°C</p>
+                </div>
+                <div className="data-column">
+                  <p>Min Temperature: {dayData.min_temp}°C</p>
+                </div>
+                <div className="data-column">
+                  <p>Sunrise: {new Date(dayData.sunrise * 1000).toLocaleTimeString()}</p>
+                </div>
+                <div className="data-column">
+                  <p>Sunset: {new Date(dayData.sunset * 1000).toLocaleTimeString()}</p>
+                </div>
+                <div className="data-column">
+                  <p>Wind Degrees: {dayData.wind_degrees}</p>
+                </div>
+                <div className="data-column">
+                  <p>Wind Speed: {dayData.wind_speed}</p>
+                </div>
+                {/* ... other weather details */}
               </div>
-              <div className="data-column">
-                <p>Feels Like: {filteredData[0].feels_like}°C</p>
-              </div>
-              {/* ... other weather details */}
-            </div>
+            ))}
           </div>
         )}
       </div>
