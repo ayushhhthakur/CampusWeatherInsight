07e0c2c81e84b10a45f66753d4035847e700492f
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-10-16 21:05:42 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -31,6 +31,37 @@ const Dashboard = () => {
   //   fetchData();
   // }, []);
 
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Jammu';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '7b3b076a32mshc6d4e9b62e1ca07p146a55jsnbc870d1b777c',
+          'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com',
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setWeatherData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   // Filter data based on the selected date
   const filteredData = weatherData?.filter((dayData) => dayData.date === selectedDate);
 
