b0cd367f957e9dd75beef6eefb5dcc88154fb6b1
Ayush Thakur <ayushthakur1412@gmail.com>
2023-11-23 14:08:53 +0530

    Updated

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,69 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Analytics.css';
+import React, { useEffect } from 'react';
+import "../styles/About.css";
 import '../styles/GlobalStyles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Analytics = () => {
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+const ComingSoon = () => {
+    useEffect(() => {
+        function updateCountdown() {
+            const currentDate = new Date();
+            const launchDate = new Date("2023-11-31T00:00:00");
+            const timeDifference = launchDate - currentDate;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Jammu';
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '7b3b076a32mshc6d4e9b62e1ca07p146a55jsnbc870d1b777c',
-          'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com',
-        },
-      };
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-      try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-          throw new Error(`Request failed with status: ${response.status}`);
+            document.getElementById("days").innerText = days.toString().padStart(2, '0');
+            document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+            document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+            document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
         }
 
-        const result = await response.json();
-        setWeatherData(result);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+        const countdownInterval = setInterval(updateCountdown, 1000);
 
-    fetchData();
-  }, []);
+        // Clean up interval on component unmount
+        return () => clearInterval(countdownInterval);
+    }, []); // Empty dependency array ensures the effect runs only once on mount
 
-  return (
-    <div className='main-content'>
-      <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-        <div className="col">
-          <div className="card mb-4 rounded-3 shadow-sm">
-            <div className="card-header py-3">
-              <h4 className="my-0 fw-normal">Temperature</h4>
+    return (
+        <div className='main-content'>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4">Coming Soon</h1>
+                        <p>We're working on something awesome! Stay tuned.</p>
+                        <div className="countdown">
+                            <div className="countdown-item">
+                                <span id="days" className="countdown-number">00</span>
+                                <span className="countdown-label">Days</span>
+                            </div>
+                            <div className="countdown-item">
+                                <span id="hours" className="countdown-number">00</span>
+                                <span className="countdown-label">Hours</span>
+                            </div>
+                            <div className="countdown-item">
+                                <span id="minutes" className="countdown-number">00</span>
+                                <span className="countdown-label">Minutes</span>
+                            </div>
+                            <div className="countdown-item">
+                                <span id="seconds" className="countdown-number">00</span>
+                                <span className="countdown-label">Seconds</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="card-body">
-              <h1 className="card-title pricing-card-title">
-                {loading ? 'Loading...' : `${weatherData?.temp || 'N/A'}`}
-                <small className="text-body-secondary fw-light">°C</small>
-              </h1>
-              <ul className="list-unstyled mt-3 mb-4">
-                <li> Clouds: {loading ? 'Loading...' : `${weatherData?.cloud_pct || 'N/A'}`} %</li>
-                <li>Min Temp: {loading ? 'Loading...' : `${weatherData?.min_temp || 'N/A'}`} °C</li>
-                {/* Add other weather-related information here */}
-              </ul>
-              <button type="button" className="w-100 btn btn-lg btn-primary">
-                Get Weather Updates
-              </button>
-            </div>
-          </div>
         </div>
-        {/* Add other cards as needed */}
-      </div>
-    </div>
-  );
+    );
 };
 
-export default Analytics;
+export default ComingSoon;
