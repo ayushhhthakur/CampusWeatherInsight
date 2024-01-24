52c01ae42b4742936ef27c0af8b47d20f6e2d4b2
Ayush Thakur <ayushthakur1412@gmail.com>
2024-01-24 15:46:33 +0530

    Add 404 page with image and styling

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+// NotFound.jsx
+
+import React from 'react';
+import '../css/NotFound.css'; // Import the CSS file for styling
+import notFound from '../assets/notfoundman.png'
 
 const NotFound = () => {
   return (
-    <div>404</div>
-  )
-}
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 style={{
+          fontSize: '3em',
+          color: '#555',
+        }}>404 - Not Found</h1>
+        <p style={{
+          fontSize: '1.2em',
+          color: '#777',
+        }}>Oops! The page you are looking for is not available.</p>
+        <img
+          src={notFound}
+          className="not-found-image"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
