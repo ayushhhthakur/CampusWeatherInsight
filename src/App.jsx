60c2fd09e6842d1c33dee76fee4b001f125440d6
Ayush Thakur <ayushthakur1412@gmail.com>
2024-01-25 16:59:28 +0530

    Update background color and add footer

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
               <Route path="/register" element={<Register />} />
             </Routes>
           </div>
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </Router>
   );
